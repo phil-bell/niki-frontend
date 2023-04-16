50d1ad5fd15e9d97810ba03a4e43d9d64b378a1c
Philip Bell <philhabell@gmail.com>
2023-04-16 19:11:54 +0100

    changing link color

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,14 @@ const userStore = useUserStore();
     <div class="wrapper">
       <nav>
         <RouterLink v-if="!userStore.authenticated" to="/">login</RouterLink>
-        <a v-else @click="userStore.logout" href="#">logout</a>
+        <a v-else @click="userStore.logout" class="nav__link" href="#"
+          >logout</a
+        >
         <span v-show="userStore.authenticated"> - </span>
-        <RouterLink v-show="userStore.authenticated" to="/search/"
+        <RouterLink
+          v-show="userStore.authenticated"
+          to="/search/"
+          class="nav__link"
           >search</RouterLink
         >
         <!-- <span v-show="userStore.authenticated"> - </span> -->
@@ -26,4 +31,11 @@ const userStore = useUserStore();
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.nav__link {
+  color: black;
+}
+.nav__link:visited {
+  color: black;
+}
+</style>
