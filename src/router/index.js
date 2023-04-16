716b20b82c52e6007d48bf9feb041a9e43322b18
Philip Bell <philhabell@gmail.com>
2023-04-16 15:44:23 +0100

    Revert "commenting history to try and make routing work on prod"
    This reverts commit 8d387fd3d94d3a1826f11f692f918a47d858a566.

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import SearchView from "../views/SearchView.vue";
 import ServerView from "../views/ServerView.vue";
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
