850851c65194cb4b982c97d45861b778abeb946c
Philip Bell <philhabell@gmail.com>
2023-04-20 00:39:09 +0100

    chaging store name of location store

--- src/stores/location.js
+++ src/stores/location.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { http } from "../utils";
 
-export const useLocationStore = defineStore("locations", {
+export const useLocationStore = defineStore("location", {
   state: () => {
     return {
       locations: null,
